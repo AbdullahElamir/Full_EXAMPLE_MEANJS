07e679bd9f9ae4abeb1d432f19733f5e75107414
abdullah Elamir <abdullah.el_ameer@yahoo.com>
2016-04-12 22:23:17 +0200

    facebook singup&login work fine

--- config/env/development.js
+++ config/env/development.js
@@ -35,8 +35,8 @@ module.exports = {
     title: defaultEnvConfig.app.title + ' - Development Environment'
   },
   facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+    clientID: process.env.FACEBOOK_ID || '962786583812172',
+    clientSecret: process.env.FACEBOOK_SECRET || '275192ab816cf9d056c76cc169220fa0',
     callbackURL: '/api/auth/facebook/callback'
   },
   twitter: {
--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -3,9 +3,42 @@
 
   angular
     .module('core')
-    .controller('HomeController', HomeController);
+      .controller('HomeController', HomeController);
 
-  function HomeController() {
+  function HomeController($scope) {
     var vm = this;
+    $scope.alerts = [{
+      total: 1800,
+      description: 'TOTAL CUSTOMERS',
+      ngClass: 'plan-name-bronze'
+    },
+    {
+      total: 8300,
+      description: 'UPCOMING EVENTS',
+      ngClass: 'plan-name-gold'
+    },
+    {
+      total: 500,
+      description: 'REFFARLS TO MODERAT',
+      ngClass: 'plan-name-silver'
+    },
+    {
+      total: 1800,
+      description: 'Half Coustomers',
+      ngClass: 'plan-name-gold'
+    },
+    {
+      total: 23232,
+      description: 'Emails Sent',
+      ngClass: 'plan-name-silver'
+    },
+    {
+      total: 8989,
+      description: 'Follow Up',
+      ngClass: 'plan-name-bronze'
+    }
+    ];
   }
+
+
 }());
